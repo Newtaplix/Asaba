1b3d99760b8e077deb499284dbd5a29e852bf8e6
Newtaplix <newton4raul@gmail.com>
2025-04-07 15:55:44 +0100

    Mover

--- components/homepage/hero.tsx
+++ components/homepage/hero.tsx
@@ -23,8 +23,7 @@ const HeroSection = () => {
             transition={{ duration: 1}} 
             whileInView="visible" 
             className="text-center md:text-left">
-                <h1 className='text-3xl font-bold flex gap-1 justify-center items-center md:mx-0 md:justify-start'>
-                  
+                <h1 className='text-3xl font-bold flex-col md:flex-row flex gap-1  justify-center items-center md:mx-0 md:justify-start'>   
                   <motion.div
                   animate={{ rotate: 360 }}
                   transition={{
@@ -33,7 +32,7 @@ const HeroSection = () => {
                     ease: "linear"
                 }}
                 className='primary rounded-full h-8 w-8 flex flex-col items-center justify-center text-white'><LucideWrench/></motion.div>
-                 Welcome to <span className="name">Asaba Auto Trading</span></h1>
+                 <span>Welcome to</span> <span className="name">Asaba Auto Trading</span></h1>
                 <p className='my-2 text-xl'>We are the solution to all your wheel problems. Making your vehicle experience seamless 
                   and breathtaking, giving your vehicle the ability to reach the highest peaks.
                 </p>
