9389a9da931dcf47192305fd8ee8d6e1a39dd8cb
Newtaplix <newton4raul@gmail.com>
2025-04-15 00:01:09 +0100

    vid

--- components/homepage/Navbar.tsx
+++ components/homepage/Navbar.tsx
@@ -1,20 +1,26 @@
+
 import React from 'react'
 
 
 const Navbar = () => {
   return (
-    <div className="flex font-bold justify-between py-4 bg-black px-6 text-white">
-      <div>
-          <h1 className='text-xl md:text-2xl'>Asaba Auto Trading</h1>
-      </div>
-      <div>
-          <ul className="flex gap-3 text-white">
-            <li>Home</li>
-            <li>Shop</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-      </div>
+    <div className='absolute w-full'>
+        <div className="flex font-bold justify-between py-4 bg-black px-6 text-white absolute top-0 w-full z-10">
+          <div>
+              <h1 className='text-xl md:text-2xl'>Asaba Auto Trading</h1>
+          </div>
+          <div>
+              <ul className="flex gap-4 text-white">
+                <li>Home</li>
+                <li>Shop</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+          </div>
+        </div>
+        <div className='w-full bg-white py-10 px-10'>
+
+        </div>
     </div>
   )
 }
--- components/homepage/hero.tsx
+++ components/homepage/hero.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
-
+import { ShoppingCart } from 'lucide-react'
+import Image from 'next/image'
 
 
 
@@ -8,18 +9,36 @@ const HeroSection = () => {
 
   return (
     <div className='h-screen w-full flex items-center'>
-        <div className="my-80 w-full text-center justify-center flex flex-col items-center h-[60%] bg-gray-300 relative">
-              <div>
-                  <h1 className="heading">Discover The Perfect</h1>
+        <div className=" w-full text-center justify-center flex flex-col items-center h-full bg-transparent relative overflow-hidden">
+             <div>
+                <video autoPlay muted loop playsInline className='w-full h-full ' src="/home/vidbg.mp4"></video>
+             </div>
+              <div className='absolute w-full h-full bg-transparent text-left flex items-center absolute px-4'>
+                <div className='w-1/2 text-white'>
+                     <div>
+                          <h1 className="font-bold text-white text-6xl italic">Built for Every Road</h1>
+                      </div>
+                      <div>
+                          <p>Trusted by Drivers Everywhere.</p>
+                      </div>
+                      <div>
+                          <p className=''>Discover durable, high performance tires for any vehicle and road condition</p>
+                      </div>
+                </div>
               </div>
-              <div>
-                  <p className='para'>Elevate your driving experience with our unique Tire collection</p>
+              <div className='absolute bottom-2 bg-white justify-center flex gap-4 w-1/2 rounded-md p-4'>
+                  <button className='px-6 py-2 rounded-md bg-black text-white font-bold'>Explore Tires</button>
+                  <button className='px-6 py-2 rounded-md bg-black text-white font-bold'>Tires</button>
+                  <button className='px-6 py-2 rounded-md bg-black text-white font-bold flex gap-2 items-center'>Shop <ShoppingCart/></button>
               </div>
-              <div className='flex gap-4'>
-                  <button className='px-6 py-5 bg-white font-bold'>Shop Now</button>
-                  <button className='px-6 py-5 bg-black text-white font-bold'>Explore Tires</button>
-                  <button className='px-6 py-5 bg-white font-bold'>Compare Tires</button>
-              </div>
+               <div className="absolute mt-4 p-2 flex gap-4 mx-auto w-[50%] justify-center">
+                    
+                    <div className="h-20 rounded-md flex-1 relative">
+                      <Image src={"/home/price1.jpg"} fill alt='svg'/>
+                    </div>
+                    
+                </div>
+
           </div>
     </div>
   )
