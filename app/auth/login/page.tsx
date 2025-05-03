2f59cff3c87db178890aaa57395a3cd853af6428
Newtaplix <newton4raul@gmail.com>
2025-05-03 03:13:22 +0100

    Api authentication complete

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,38 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const LoginPage = () => {
+    const [username, setUsername] = useState<string>()
+    const [password, setPassword] = useState<string>()
+    const [tokens, setTokens] = useState({access: "" , refresh: ""})
+    const router = useRouter()
+    const BASEURL = "http://127.0.0.1:8000/"
+
+    const login = async (e:React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        try{
+            const data = await axios.post(`${BASEURL}api/token/`, {
+                username: username,
+                password: password
+            })
+            console.log(data.data)
+            setTokens({access: data.data.access, refresh:data.data.refresh})
+            router.push("/")
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        if(tokens){
+            localStorage.setItem("Aaccess", tokens.access)
+            localStorage.setItem("Arefresh", tokens.access)
+        }
+    }, [tokens])
   return (
     <div className="flex row-reverse h-screen">
         <div className='w-full bg-[url("/sideImg.png")] bg-cover bg-center h-full flex items-center justify-center'>
@@ -15,10 +45,10 @@ const LoginPage = () => {
                     <Button className='w-full mt-6 bg-gray-200 border-2 border-gray-400 text-black hover:bg-gray-500 mb-3'>Continue with Google</Button>
                     <hr/>
                     <div>
-                        <Input type='text' placeholder='Username' className='p-2 mt-3 border-none bg-black/40'/>
-                        <Input type='password' placeholder='Password' className='p-2 mt-3 border-none bg-black/40'/>
+                        <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' className='p-2 mt-3 border-none bg-black/40'/>
+                        <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='p-2 mt-3 border-none bg-black/40'/>
                     </div>
-                    <Button className='cursor-pointer w-full bg-white hover:bg-gray-300 text-black mt-3'>Log In</Button>
+                    <Button onClick={(e) => login(e)} className='cursor-pointer w-full bg-white hover:bg-gray-300 text-black mt-3'>Log In</Button>
                     <div className='text-center justify-center mt-3 w-full'>
                         <p className='text-gray-500 w-full mx-auto text-center flex gap-2'><span>Don&lsquo;t have an account? </span>
                         <span className='text-gray-600 hover:text-white cursor-pointer'>Sign Up</span></p>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,8 +1,29 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const SignUpPage = () => {
+    const [username, setUsername]=useState<string>("")
+    const [password, setPassword] =useState<string>("")
+    const [email, setEmail]=useState<string>("")
+    const router = useRouter()
+    const signup = async (e:React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        try{
+            const data = await axios.post("http://127.0.0.1:8000/api/register/", {
+                username: username,
+                email: email,
+                password: password
+            })
+            console.log(data.data)
+            router.push("/auth/login")
+        }catch(e){
+            console.log(e)
+        }
+    }
   return (
     <div className="flex row-reverse h-screen">
         <div className='w-full bg-[url("/sideImg.png")] bg-cover bg-center h-full flex items-center justify-center'>
@@ -15,11 +36,11 @@ const SignUpPage = () => {
                     <Button className='w-full mt-6 bg-gray-200 border-2 border-gray-400 text-black hover:bg-gray-500 mb-3'>Continue with Google</Button>
                     <hr/>
                     <div>
-                        <Input type='text' placeholder='Username' className='p-2 mt-3 border-none bg-black/40'/>
-                        <Input type='email' placeholder='Email' className='p-2 mt-3 border-none bg-black/40'/>
-                        <Input type='password' placeholder='Password' className='p-2 mt-3 border-none bg-black/40'/>
+                        <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' className='p-2 mt-3 border-none bg-black/40'/>
+                        <Input type='email' onChange={(e) => setEmail(e.target.value)} placeholder='Email' className='p-2 mt-3 border-none bg-black/40'/>
+                        <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='p-2 mt-3 border-none bg-black/40'/>
                     </div>
-                    <Button className='cursor-pointer w-full bg-white hover:bg-gray-300 text-black mt-3'>Sign Up</Button>
+                    <Button onClick={(e) => signup(e)} className='cursor-pointer w-full bg-white hover:bg-gray-300 text-black mt-3'>Sign Up</Button>
                     <div className='text-center justify-center mt-3 w-full'>
                         <p className='text-gray-500 w-full mx-auto text-center flex gap-2'><span>Already have an account? </span>
                         <span className='text-gray-600 hover:text-white cursor-pointer'>login</span></p>
